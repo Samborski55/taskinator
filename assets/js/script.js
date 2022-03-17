7f63cbec93539bcdcb1f1d024b599d1cd0948ebf
Norse <cody.samborski@gmail.com>
2022-03-17 15:27:29 -0500

    allowed deletions in localstorage array

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
 var pageContentEl = document.querySelector("#page-content");
 
+var tasks = [];
+
 var taskFormHandler = function(event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
@@ -30,9 +32,11 @@ var taskFormHandler = function(event) {
   } else {
     var taskDataObj = {
       name: taskNameInput,
-      type: taskTypeInput
+      type: taskTypeInput,
+      status: "to do"
     };
-
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
     createTaskEl(taskDataObj);
   }
 };
@@ -51,7 +55,8 @@ var createTaskEl = function(taskDataObj) {
   var taskActionsEl = createTaskActions(taskIdCounter);
   listItemEl.appendChild(taskActionsEl);
   tasksToDoEl.appendChild(listItemEl);
-
+  taskDataObj.id = taskIdCounter;
+  tasks.push(taskDataObj);
   // increase task counter for next unique id
   taskIdCounter++;
 };
@@ -103,6 +108,13 @@ var completeEditTask = function(taskName, taskType, taskId) {
   taskSelected.querySelector("h3.task-name").textContent = taskName;
   taskSelected.querySelector("span.task-type").textContent = taskType;
 
+  for (var i = 0; i < tasks.length; i++) {
+      if (tasks[i].id === parseInt(taskId)) {
+          tasks[i].name = taskName;
+          tasks[i].type = taskType;
+      }
+  };
+
   alert("Task Updated!");
 
   // remove data attribute from form
@@ -144,6 +156,14 @@ var taskStatusChangeHandler = function(event) {
   } else if (statusValue === "completed") {
     tasksCompletedEl.appendChild(taskSelected);
   }
+
+  // update task's in tasks array
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+            tasks[i].satus = statusValue;
+        }
+    }
+    console.log(tasks)
 };
 
 var editTask = function(taskId) {
